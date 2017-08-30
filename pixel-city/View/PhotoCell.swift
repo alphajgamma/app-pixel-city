044fd49e67572ebf590c0cbf27916fd9b8c24b16
Andrew Greenough <andygreenough@gmail.com>
2017-08-30 10:12:16 +0100

    Adding UILabel for Pull Up View / Adding UICollectionView Programmatically

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Andrew Greenough on 30/08/2017.
+//  Copyright © 2017 Andrew Greenough. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
